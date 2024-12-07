f2a18a811dca9ce80f6d4776e8563c9148467dbf
allavinner <joel.w.ottosson@gmail.com>
2024-12-07 10:29:43 +0100

    Add compiled wasm binaries.

---
+++ wasm-src/pkg/wasm_src.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export function greet(): void;
+export function test_fn(): string;
+export function test_err(): string;
+export function solve(input: string, day: number, part: number): string;
